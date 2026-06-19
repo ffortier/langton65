7b76b22bbdc7016cde5c8ab14427363849793a31
Francis Fortier <francis.fortier@gmail.com>
2026-06-19 10:52:40 -0400

    attempt smc to speed-up the match function

--- src/match.s
+++ src/match.s
@@ -1,10 +1,83 @@
 
-.importzp tmp1, tmp3, ptr1, sreg
+.importzp tmp1, tmp3, ptr1, sreg, rotation_counter, sz
 .importzp ruleset_lookup
 .import _rotate, _dump
 .export _match
 
-.macro ldsz
+SMCPTR = $d000
+
+.ifndef SMC
+.proc _match
+    sta tmp1
+    lda #4          ; Init rotation counter in tmp3
+    sta rotation_counter
+    lda tmp1
+
+    asl             ; Lookup ruleset address and modify the code
+    tax
+    lda ruleset_lookup, X
+    sta @smc1 + 1
+    sta @smc2 + 1
+    sta @smc3 + 1
+    sta @smc4 + 1
+    lda ruleset_lookup + 1, X
+    sta @smc1 + 2
+    sta @smc2 + 2
+    sta @smc3 + 2
+    sta @smc4 + 2
+    ldy #0
+@smc1:
+    ldx SMCPTR, Y
+    stx sz
+
+@again:
+    ldx sz
+    ldy #1
+
+@loop:
+@smc2:
+    lda SMCPTR, Y
+    and #$f8
+    cmp sreg
+    bne @next
+    jmp @found
+@next:
+    iny
+@next1:
+    iny
+    dex
+    bne @loop
+@end:
+    dec rotation_counter
+    bne @retry
+    lda tmp1
+    rts
+@retry:
+    lda tmp1
+    jsr _rotate
+    jmp @again
+@found:
+    iny
+@smc3:
+    lda SMCPTR, Y
+    cmp sreg + 1
+    bne @next1
+    dey
+@smc4:
+    lda SMCPTR, Y
+    and #$07
+    rts
+
+.endproc
+
+.else
+
+.proc _match
+    sta tmp1
+    lda #4
+    sta rotation_counter
+    lda tmp1
+
     asl
     tax
     lda ruleset_lookup, X
@@ -15,16 +88,11 @@
     lda (ptr1), Y
     iny
     tax
-.endmacro
-
-.proc _match
-    sta tmp1
-    lda #4
-    sta tmp3
-    lda tmp1
+    stx sz
 
 @again:
-    ldsz
+    ldx sz
+    ldy #1
 
 @loop:
     lda (ptr1), Y
@@ -39,7 +107,7 @@
     dex
     bne @loop
 @end:
-    dec tmp3
+    dec rotation_counter
     bne @retry
     lda tmp1
     rts
@@ -57,3 +125,4 @@
     and #$07
     rts
 .endproc
+.endif
--- src/zeropage.s
+++ src/zeropage.s
@@ -1,6 +1,8 @@
-.exportzp ruleset_lookup, current_buf, next_buf
+.exportzp ruleset_lookup, current_buf, next_buf, rotation_counter, sz
 
 .zeropage
 ruleset_lookup: .res 16
 current_buf: .res 2
 next_buf: .res 2
+rotation_counter: .res 1
+sz: .res 1
